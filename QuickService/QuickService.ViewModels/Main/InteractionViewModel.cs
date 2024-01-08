946a4c0c6fcd19b8c8a78cada5fe049e3bee249d
KR-JsonLane <malbox5034@naver.com>
2024-01-08 21:31:52 +0900

    InteractionView작업중
    * 각 역할에 따른 버튼 추가

--- QuickService/QuickService.ViewModels/Main/InteractionViewModel.cs
+++ QuickService/QuickService.ViewModels/Main/InteractionViewModel.cs
@@ -15,5 +15,29 @@ public partial class InteractionViewModel : ObservableRecipient, IViewModel
 
     #region Methods
 
+    /// <summary>
+    /// URL파라미터 오픈
+    /// </summary>
+    /// <param name="url"> 오픈할 URL </param>
+    [RelayCommand]
+    private void OpenWebSiteLink(string url)
+	{
+		var sInfo = new ProcessStartInfo(url)
+		{
+			UseShellExecute = true,
+		};
+		Process.Start(sInfo);
+	}
+
+    /// <summary>
+    /// 사용자가 선택한 어플리케이션을 기억
+    /// </summary>
+    /// <param name="param"> LEFT, TOP, RIGHT, BOTTOM 파라미터 </param>
+    [RelayCommand]
+    private void MemorizedQuickServiceApplication(string param)
+    {
+        // TODO : 파라미터에 따른 위치의 어플리케이션을 등록.
+    }
+
     #endregion
 }
