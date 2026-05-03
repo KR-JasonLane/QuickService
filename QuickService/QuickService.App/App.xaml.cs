6f206943ca6948830cdd382902e9fbb99ba5ebe2
Lee Coder <98294800+KR-JasonLane@users.noreply.github.com>
2026-05-03 14:07:24 +0900

    REFACTOR : IocBuilder 구조화 및 App.xaml.cs 메서드 분리

--- QuickService/QuickService.App/App.xaml.cs
+++ QuickService/QuickService.App/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using QuickService.Abstract.Interfaces;
+using QuickService.Abstract.Interfaces;
 using QuickService.ViewModels;
 using QuickService.Views;
 
@@ -14,60 +14,43 @@ public partial class App : Application
 		IocBuilder.Build();
 	}
 
-	#region Override Methods
-
-	/// <summary>
-	/// 시스템 시작
-	/// </summary>
-	/// <param name="e"> 이벤트 파라미터 </param>
 	protected override void OnStartup(StartupEventArgs e)
 	{
 		base.OnStartup(e);
-
-		////////////////////////////////////////
-		// Global Hook
-		////////////////////////////////////////
-		{
-			Ioc.Default.GetService<IGlobalMouseHookService   >()!.SetHook();
-			Ioc.Default.GetService<IGlobalKeyboardHookService>()!.SetHook();
-		}
-
-
-		////////////////////////////////////////
-		// Shell Window
-		////////////////////////////////////////
-		{
-			ShellWindow shellwindow = new() { DataContext = Ioc.Default.GetService<ShellWindowViewModel>() };
-			shellwindow.Show();
-
-			Current.MainWindow = shellwindow;
-		}
-
-		////////////////////////////////////////
-		// Selected Launch App Window
-		////////////////////////////////////////
-		{
-			SelectLaunchAppWindow selectLaunchAppWindow = new() { DataContext = Ioc.Default.GetService<SelectLaunchAppWindowViewModel>() };
-			selectLaunchAppWindow.Show();
-		}
+		SetupGlobalHooks();
+		ShowMainWindow();
+		ShowSelectionWindow();
 	}
 
-	/// <summary>
-	/// 시스템 종료
-	/// </summary>
-	/// <param name="e"> 이벤트 파라미터 </param>
 	protected override void OnExit(ExitEventArgs e)
 	{
 		base.OnExit(e);
-
-		////////////////////////////////////////
-		// Global Hook
-		////////////////////////////////////////
-		{
-			Ioc.Default.GetService<IGlobalMouseHookService	 >()!.UnHook();
-			Ioc.Default.GetService<IGlobalKeyboardHookService>()!.UnHook();
-		}
+		Ioc.Default.GetService<IGlobalMouseHookService>()!.UnHook();
+		Ioc.Default.GetService<IGlobalKeyboardHookService>()!.UnHook();
 	}
 
-	#endregion
+	private static void SetupGlobalHooks()
+	{
+		Ioc.Default.GetService<IGlobalMouseHookService>()!.SetHook();
+		Ioc.Default.GetService<IGlobalKeyboardHookService>()!.SetHook();
+	}
+
+	private void ShowMainWindow()
+	{
+		var shellWindow = new ShellWindow
+		{
+			DataContext = Ioc.Default.GetService<ShellWindowViewModel>()
+		};
+		shellWindow.Show();
+		Current.MainWindow = shellWindow;
+	}
+
+	private static void ShowSelectionWindow()
+	{
+		var selectWindow = new SelectLaunchAppWindow
+		{
+			DataContext = Ioc.Default.GetService<SelectLaunchAppWindowViewModel>()
+		};
+		selectWindow.Show();
+	}
 }
--- QuickService/QuickService.App/MVVM/IocBuilder.cs
+++ QuickService/QuickService.App/MVVM/IocBuilder.cs
@@ -1,4 +1,4 @@
-﻿using QuickService.Abstract.Interfaces;
+using QuickService.Abstract.Interfaces;
 using QuickService.Core.Services;
 using QuickService.ViewModels;
 
@@ -6,67 +6,37 @@ namespace QuickService.App;
 
 public static class IocBuilder
 {
-
 	/// <summary>
 	/// IoC 컨테이너 빌드
 	/// </summary>
 	public static void Build()
 	{
-		var services = ConfigureService();
-
-		Ioc.Default.ConfigureServices(services);
+		var services = new ServiceCollection();
+		RegisterServices(services);
+		RegisterViewModels(services);
+		Ioc.Default.ConfigureServices(services.BuildServiceProvider());
 	}
 
-	/// <summary>
-	/// Service Provider 설정
-	/// </summary>
-	/// <returns></returns>
-	private static IServiceProvider ConfigureService()
+	private static void RegisterServices(IServiceCollection services)
 	{
-		var services = new ServiceCollection();
+		services.AddSingleton<IJsonFileService, JsonFileService>();
+		services.AddSingleton<IConfigurationService, ConfigurationService>();
+		services.AddSingleton<ILaunchAppService, LaunchAppService>();
+		services.AddSingleton<IGlobalMouseHookService, GlobalMouseHookService>();
+		services.AddSingleton<IGlobalKeyboardHookService, GlobalKeyboardHookService>();
+		services.AddSingleton<IHideMainWindowService, HideMainWindowService>();
+		services.AddSingleton<ITrayIconService, TrayIconService>();
+		services.AddSingleton<IUserSelectPathService, UserSelectPathService>();
+		services.AddSingleton<IDialogHostService, DialogHostService>();
+	}
 
-		////////////////////////////////////////
-		// Services
-		////////////////////////////////////////
-		{
-			services.AddSingleton<IHideMainWindowService     , HideMainWindowService     >();
-			services.AddSingleton<ITrayIconService           , TrayIconService           >();
-			services.AddSingleton<IUserSelectPathService     , UserSelectPathService     >();
-			services.AddSingleton<IJsonFileService           , JsonFileService           >();
-			services.AddSingleton<IConfigurationService      , ConfigurationService      >();
-			services.AddSingleton<IGlobalMouseHookService    , GlobalMouseHookService    >();
-			services.AddSingleton<IGlobalKeyboardHookService , GlobalKeyboardHookService >();
-			services.AddSingleton<ILaunchAppService          , LaunchAppService          >();
-			services.AddSingleton<IDialogHostService         , DialogHostService         >();
-		}
-
-
-		////////////////////////////////////////
-		// Shell Window
-		////////////////////////////////////////
-		{
-			services.AddTransient<ShellWindowViewModel>();
-		}
-
-
-		////////////////////////////////////////
-		// Select Launch App Window
-		////////////////////////////////////////
-		{
-			services.AddTransient<SelectLaunchAppWindowViewModel>();
-		}
-
-
-		////////////////////////////////////////
-		// Shell Contents
-		////////////////////////////////////////
-		{
-			services.AddTransient<MainViewModel			>();
-			services.AddTransient<TitleViewModel		>();
-			services.AddTransient<InteractionViewModel	>();
-			services.AddTransient<SelectedFileViewModel	>();
-		}
-
-		return services.BuildServiceProvider();
+	private static void RegisterViewModels(IServiceCollection services)
+	{
+		services.AddTransient<ShellWindowViewModel>();
+		services.AddTransient<MainViewModel>();
+		services.AddTransient<TitleViewModel>();
+		services.AddTransient<InteractionViewModel>();
+		services.AddTransient<SelectedFileViewModel>();
+		services.AddTransient<SelectLaunchAppWindowViewModel>();
 	}
 }
