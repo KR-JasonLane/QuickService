4ec24c02bb0a5016b62498a2ea3cef02ebf858c4
Lee Coder <98294800+KR-JasonLane@users.noreply.github.com>
2026-05-03 13:41:49 +0900

    REFACTOR : TitleViewModel 리팩토링 - 불필요 블록 제거 및 코드 정리

--- QuickService/QuickService.ViewModels/Main/TitleViewModel.cs
+++ QuickService/QuickService.ViewModels/Main/TitleViewModel.cs
@@ -1,71 +1,47 @@
-﻿using QuickService.Abstract.Interfaces;
+using QuickService.Abstract.Interfaces;
 using QuickService.ViewModels.Main;
 
 namespace QuickService.ViewModels;
+
 public partial class TitleViewModel : ObservableRecipient, IViewModel
 {
-    public TitleViewModel(ITrayIconService trayIconService, IHideMainWindowService hideMainWindowService, IDialogHostService dialogHostService)
-    {
-        ////////////////////////////////////////
-        // 서비스 등록
-        ////////////////////////////////////////
-        {
-            _trayIconService        = trayIconService;
-            _hideMainWindowService  = hideMainWindowService;
-			_dialogHostService      = dialogHostService;
-		}
-    }
-
-    #region Properties
-
-    /// <summary>
-    /// 트레이 아이콘 서비스
-    /// </summary>
-    private readonly ITrayIconService _trayIconService;
-
-    /// <summary>
-    /// 메인윈도우 Hide 서비스
-    /// </summary>
-    private readonly IHideMainWindowService _hideMainWindowService;
-
-	/// <summary>
-	/// 다이얼로그 호스트 서비스
-	/// </summary>
+	private readonly ITrayIconService _trayIconService;
+	private readonly IHideMainWindowService _hideMainWindowService;
 	private readonly IDialogHostService _dialogHostService;
 
-	#endregion
-
-	#region Commands
+	public TitleViewModel(
+		ITrayIconService trayIconService,
+		IHideMainWindowService hideMainWindowService,
+		IDialogHostService dialogHostService)
+	{
+		_trayIconService       = trayIconService;
+		_hideMainWindowService = hideMainWindowService;
+		_dialogHostService     = dialogHostService;
+	}
 
 	/// <summary>
 	/// 프로세스 종료 커맨드
 	/// </summary>
 	[RelayCommand]
-    private void KillProcess() => Application.Current.Shutdown();
+	private void KillProcess() => Application.Current.Shutdown();
 
-    /// <summary>
-    /// 메인윈도우를 숨기고 트레이 아이콘 설치
-    /// </summary>
-    [RelayCommand]
-    private void HideWindowAndVisibleTrayIcon()
-    {
-        _hideMainWindowService.HideMainWindow();
-        _trayIconService.VisibleTrayIconOnTaskBar(true);
+	/// <summary>
+	/// 메인윈도우를 숨기고 트레이 아이콘 설치
+	/// </summary>
+	[RelayCommand]
+	private void HideWindowAndVisibleTrayIcon()
+	{
+		_hideMainWindowService.HideMainWindow();
+		_trayIconService.VisibleTrayIconOnTaskBar(true);
 	}
 
-    /// <summary>
-    /// 옵션 다이얼로그 표시
-    /// </summary>
-    [RelayCommand]
-    private void ShowOptionDialog()
-    {
-        var content = new ConfigDialogViewModel();
-        _dialogHostService.ShowDialog("ShellWindowHost", content);
-    }
-
-	#endregion
-
-	#region Methods
-
-	#endregion
+	/// <summary>
+	/// 옵션 다이얼로그 표시
+	/// </summary>
+	[RelayCommand]
+	private void ShowOptionDialog()
+	{
+		var content = new ConfigDialogViewModel();
+		_dialogHostService.ShowDialog("ShellWindowHost", content);
+	}
 }
