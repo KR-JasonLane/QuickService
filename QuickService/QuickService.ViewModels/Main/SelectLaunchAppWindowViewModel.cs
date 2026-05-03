f24820d6a6c7f919e978209ce09c178ed50d1d88
Lee Coder <98294800+KR-JasonLane@users.noreply.github.com>
2026-05-03 14:03:59 +0900

    REFACTOR : SelectLaunchAppWindowViewModel 리팩토링 - 생성자 분리, 매직넘버 상수화, GetByPosition 활용

--- QuickService/QuickService.ViewModels/Main/SelectLaunchAppWindowViewModel.cs
+++ QuickService/QuickService.ViewModels/Main/SelectLaunchAppWindowViewModel.cs
@@ -1,306 +1,193 @@
-﻿using QuickService.Abstract.Interfaces;
+using QuickService.Abstract.Interfaces;
 using QuickService.Models.Configure;
 using QuickService.ViewModels.Messenger;
+using QuickService.ViewModels.Messenger.Parameters;
 
 namespace QuickService.ViewModels;
 
 public partial class SelectLaunchAppWindowViewModel : ObservableRecipient, IViewModel
 {
-	public SelectLaunchAppWindowViewModel(IConfigurationService configurationService, ILaunchAppService launchAppService)
+	private const double SelectionWindowSize = 300;
+	private const double InvalidAreaSize     = 52;
+	private const double RightTopBoundary    = 45;
+	private const double TopLeftBoundary     = 135;
+	private const double LeftBottomBoundary  = 225;
+	private const double BottomRightBoundary = 315;
+
+	private readonly IConfigurationService _configurationService;
+	private readonly ILaunchAppService _launchAppService;
+	private Point _currentWindowCenter;
+
+	public SelectLaunchAppWindowViewModel(
+		IConfigurationService configurationService,
+		ILaunchAppService launchAppService)
 	{
-		////////////////////////////////////////
-		// 서비스 등록
-		////////////////////////////////////////
+		_configurationService = configurationService;
+		_launchAppService     = launchAppService;
+
+		WindowLength        = SelectionWindowSize;
+		InvalidAreaDiameter = InvalidAreaSize;
+
+		RegisterMessengers();
+		LoadAllAppIcons();
+	}
+
+	#region Observable Properties
+
+	[ObservableProperty] private bool _isSelectedLeft;
+	[ObservableProperty] private bool _isSelectedTop;
+	[ObservableProperty] private bool _isSelectedRight;
+	[ObservableProperty] private bool _isSelectedBottom;
+	[ObservableProperty] private bool _isNoneSelect;
+	[ObservableProperty] private double _pointerAngle;
+	[ObservableProperty] private bool _isWindowOpen;
+	[ObservableProperty] private ImageSource? _leftAppIconImageSource;
+	[ObservableProperty] private ImageSource? _topAppIconImageSource;
+	[ObservableProperty] private ImageSource? _rightAppIconImageSource;
+	[ObservableProperty] private ImageSource? _bottomAppIconImageSource;
+	[ObservableProperty] private double _positionLeft;
+	[ObservableProperty] private double _positionTop;
+	[ObservableProperty] private double _windowLength;
+	[ObservableProperty] private double _invalidAreaDiameter;
+
+	#endregion
+
+	private void RegisterMessengers()
+	{
+		WeakReferenceMessenger.Default.Register<MouseClickStateMessage>(this, (r, m) =>
+			HandleMouseClick(m.Value));
+		WeakReferenceMessenger.Default.Register<ModifierKeyStateMessage>(this, (r, m) =>
 		{
-			_configurationService = configurationService;
-			_launchAppService     = launchAppService;
+			if (!m.Value) IsWindowOpen = false;
+		});
+		WeakReferenceMessenger.Default.Register<MouseMoveMessage>(this, (r, m) =>
+		{
+			if (IsWindowOpen) HandleMouseMove(new Point(m.Value.X, m.Value.Y));
+		});
+		WeakReferenceMessenger.Default.Register<AppInformationChangedMessage>(this, (r, m) =>
+			LoadAppIcon(m.Value));
+	}
+
+	private void HandleMouseClick(MouseClickParameter click)
+	{
+		IsWindowOpen = click.IsDown;
+		IsNoneSelect = true;
+
+		if (click.IsDown)
+		{
+			PositionLeft = click.X - (WindowLength / 2);
+			PositionTop  = click.Y - (WindowLength / 2);
+			_currentWindowCenter = new Point(click.X, click.Y);
+			HandleMouseMove(_currentWindowCenter);
 		}
-
-
-		////////////////////////////////////////
-		// 메신저 등록
-		////////////////////////////////////////
+		else
 		{
-			WeakReferenceMessenger.Default.Register<MouseClickStateMessage>(this, (r, m) =>
-			{
-				IsWindowOpen   = m.Value.IsDown;
-				IsNoneSelect   = true          ;
-
-				if (m.Value.IsDown)
-				{
-					PositionLeft = m.Value.X - (WindowLength / 2);
-					PositionTop  = m.Value.Y - (WindowLength / 2);
-
-					_currentWindowCenter = new Point(m.Value.X, m.Value.Y);
-
-					CalculateMousePoint(_currentWindowCenter);
-				}
-				else
-				{
-					LaunchSelectedApp();
-				}
-			});
-
-			WeakReferenceMessenger.Default.Register<ModifierKeyStateMessage>(this, (r, m) =>
-			{
-				if (m.Value is false)
-					IsWindowOpen = false;
-			});
-
-			WeakReferenceMessenger.Default.Register<MouseMoveMessage>(this, (r, m) =>
-			{
-				if (IsWindowOpen) 
-					CalculateMousePoint(new Point(m.Value.X, m.Value.Y));
-			});
-
-			WeakReferenceMessenger.Default.Register<AppInformationChangedMessage>(this, (r, m) =>
-			{
-				LoadUserSelectedAppIconImages(m.Value);
-			});
-		}
-
-
-		////////////////////////////////////////
-		// 바인딩 속성
-		////////////////////////////////////////
-		{
-			WindowLength        = 300; //윈도우 크기
-			InvalidAreaDiameter = 52 ; //무효영역 크기
-
-			LoadUserSelectedAppIconImages(AppPosition.Left  );
-			LoadUserSelectedAppIconImages(AppPosition.Top   );
-			LoadUserSelectedAppIconImages(AppPosition.Right );
-			LoadUserSelectedAppIconImages(AppPosition.Bottom);
+			LaunchSelectedApp();
 		}
 	}
 
-	#region Properties
-
-	/// <summary>
-	/// 사용자 설정 서비스
-	/// </summary>
-	private IConfigurationService _configurationService;
-
-	/// <summary>
-	/// 어플리케이션 실행 서비스
-	/// </summary>
-	private ILaunchAppService _launchAppService;
-
-	/// <summary>
-	/// 좌측 앱 선택
-	/// </summary>
-	[ObservableProperty]
-	private bool _isSelectedLeft;
-
-	/// <summary>
-	/// 상단 앱 선택
-	/// </summary>
-	[ObservableProperty]
-	private bool _isSelectedTop;
-
-	/// <summary>
-	/// 우측 앱 선택
-	/// </summary>
-	[ObservableProperty]
-	private bool _isSelectedRight;
-
-	/// <summary>
-	/// 하단 앱 선택
-	/// </summary>
-	[ObservableProperty]
-	private bool _isSelectedBottom;
-
-	/// <summary>
-	/// 선택 안함
-	/// </summary>
-	[ObservableProperty]
-	private bool _isNoneSelect;
-
-
-	/// <summary>
-	/// 마우스가 가리키고 있는 각도
-	/// </summary>
-	[ObservableProperty]
-	private double _pointerAngle;
-
-	/// <summary>
-	/// 윈도우가 Show됨을 알림
-	/// </summary>
-	[ObservableProperty]
-	private bool _isWindowOpen;
-
-
-	/// <summary>
-	/// 좌측 앱 아이콘 이미지
-	/// </summary>
-	[ObservableProperty]
-	private ImageSource? _leftAppIconImageSource;
-
-	/// <summary>
-	/// 상단 앱 아이콘 이미지
-	/// </summary>
-	[ObservableProperty]
-	private ImageSource? _topAppIconImageSource;
-
-	/// <summary>
-	/// 우측 앱 아이콘 이미지
-	/// </summary>
-	[ObservableProperty]
-	private ImageSource? _rightAppIconImageSource;
-
-	/// <summary>
-	/// 하단 앱 아이콘 이미지
-	/// </summary>
-	[ObservableProperty]
-	private ImageSource? _bottomAppIconImageSource;
-
-	/// <summary>
-	/// 윈도우의 Left좌표
-	/// </summary>
-	[ObservableProperty]
-	private double _positionLeft;
-
-	/// <summary>
-	/// 윈도우의 Top좌표
-	/// </summary>
-	[ObservableProperty]
-	private double _positionTop;
-
-	/// <summary>
-	/// 윈도우의 가로,세로 길이
-	/// </summary>
-	[ObservableProperty]
-	private double _windowLength;
-
-	/// <summary>
-	/// 무효영역의 지름
-	/// </summary>
-	[ObservableProperty]
-	private double _invalidAreaDiameter;
-
-	/// <summary>
-	/// 현재 윈도우의 가운데 좌표 저장
-	/// </summary>
-	private Point _currentWindowCenter;
-
-	#endregion
-
-	#region Commands
-
-	#endregion
-
-	#region Methods
-
-	/// <summary>
-	/// 마우스 좌표를 계산
-	/// </summary>
-	/// <param name="mousePosition"> 현재 마우스의 위치 </param>
-	private void CalculateMousePoint(Point mousePosition)
+	private void HandleMouseMove(Point mousePosition)
 	{
-		if(CheckMouseInInvalidArea(mousePosition))
+		if (IsWithinInvalidArea(mousePosition))
 		{
-			TurnOffSelectedFlags();
+			ClearSelection();
 			IsNoneSelect = true;
 			return;
 		}
 
 		IsNoneSelect = false;
-
-		double deltaX = mousePosition       .X - _currentWindowCenter.X;
-		double deltaY = _currentWindowCenter.Y - mousePosition       .Y;
-		double angle = Math.Atan2(deltaY, deltaX) * (180 / Math.PI);
-
+		var angle = CalculateAngle(mousePosition);
 		PointerAngle = -angle + 90;
-
-		CalculatePointedPosition(angle);
+		ApplySelection(DeterminePosition(angle));
 	}
 
-	/// <summary>
-	/// 마우스가 무효영역에 있는지 확인
-	/// </summary>
-	/// <param name="mouse"> 현재 마우스 위치 </param>
-	/// <returns> 마우스가 무효영역에 위치해 있는지 여부</returns>
-	private bool CheckMouseInInvalidArea(Point mousePosition)
+	private bool IsWithinInvalidArea(Point mouse)
 	{
-		double ellipseRadiusX = InvalidAreaDiameter / 2;
-		double ellipseRadiusY = InvalidAreaDiameter / 2;
-
-		double distance = Math.Sqrt(Math.Pow(mousePosition.X - _currentWindowCenter.X, 2) / Math.Pow(ellipseRadiusX, 2) +
-									Math.Pow(mousePosition.Y - _currentWindowCenter.Y, 2) / Math.Pow(ellipseRadiusY, 2));
-
-		return distance <= 1.0;
+		var radius = InvalidAreaDiameter / 2;
+		var dx = (mouse.X - _currentWindowCenter.X) / radius;
+		var dy = (mouse.Y - _currentWindowCenter.Y) / radius;
+		return (dx * dx + dy * dy) <= 1.0;
 	}
 
-	/// <summary>
-	/// 마우스가 가리키고있는 영역을 계산
-	/// </summary>
-	/// <param name="angle"> 마우스의 각도</param>
-	private void CalculatePointedPosition(double angle)
+	private double CalculateAngle(Point mouse)
 	{
-		TurnOffSelectedFlags();
+		var deltaX = mouse.X - _currentWindowCenter.X;
+		var deltaY = _currentWindowCenter.Y - mouse.Y;
+		return Math.Atan2(deltaY, deltaX) * (180 / Math.PI);
+	}
 
-		if(angle < 0) angle += 360;
-
-		IsSelectedTop    = angle >= 45  && angle < 135;
-		IsSelectedLeft   = angle >= 135 && angle < 225;
-		IsSelectedBottom = angle >= 225 && angle < 315;
-		IsSelectedRight  = angle >= 315 || angle < 45;
-
-		if (IsSelectedLeft || IsSelectedTop || IsSelectedRight || IsSelectedBottom)
+	private static AppPosition DeterminePosition(double angle)
+	{
+		if (angle < 0) angle += 360;
+		return angle switch
 		{
-			IsNoneSelect = false;
+			>= BottomRightBoundary or < RightTopBoundary => AppPosition.Right,
+			>= RightTopBoundary and < TopLeftBoundary    => AppPosition.Top,
+			>= TopLeftBoundary and < LeftBottomBoundary  => AppPosition.Left,
+			_                                            => AppPosition.Bottom,
+		};
+	}
+
+	private void ApplySelection(AppPosition position)
+	{
+		ClearSelection();
+		switch (position)
+		{
+			case AppPosition.Left:   IsSelectedLeft   = true; break;
+			case AppPosition.Top:    IsSelectedTop    = true; break;
+			case AppPosition.Right:  IsSelectedRight  = true; break;
+			case AppPosition.Bottom: IsSelectedBottom = true; break;
 		}
 	}
 
-	/// <summary>
-	/// 모든 선택플래그를 끔
-	/// </summary>
-	private void TurnOffSelectedFlags()
+	private void ClearSelection()
 	{
-		IsSelectedLeft   = false;
-		IsSelectedTop    = false;
-		IsSelectedRight  = false;
-		IsSelectedBottom = false;
+		IsSelectedLeft = IsSelectedTop = IsSelectedRight = IsSelectedBottom = false;
 	}
 
-	/// <summary>
-	/// 사용자가 선택한 어플리케이션의 아이콘을 이미지로 저장
-	/// </summary>
-	/// <param name="position"> 설정 하고자 하는 영역 </param>
-	private void LoadUserSelectedAppIconImages(AppPosition position)
+	private void LoadAllAppIcons()
 	{
-		var configuration = _configurationService.GetConfiguration<RegisteredApplicationModel>();
-		if (configuration is null) return;
+		foreach (var position in Enum.GetValues<AppPosition>())
+			LoadAppIcon(position);
+	}
 
-		var appInfo = configuration.GetByPosition(position);
+	private void LoadAppIcon(AppPosition position)
+	{
+		var config = _configurationService.GetConfiguration<RegisteredApplicationModel>();
+		if (config is null) return;
+
+		var appInfo = config.GetByPosition(position);
 		if (!appInfo.HasValidPath()) return;
 
 		appInfo.LoadInfoFromPath();
-
 		switch (position)
 		{
-			case AppPosition.Left:   LeftAppIconImageSource   = appInfo.IconImage!; break;
-			case AppPosition.Top:    TopAppIconImageSource    = appInfo.IconImage!; break;
-			case AppPosition.Right:  RightAppIconImageSource  = appInfo.IconImage!; break;
-			case AppPosition.Bottom: BottomAppIconImageSource = appInfo.IconImage!; break;
+			case AppPosition.Left:   LeftAppIconImageSource   = appInfo.IconImage; break;
+			case AppPosition.Top:    TopAppIconImageSource    = appInfo.IconImage; break;
+			case AppPosition.Right:  RightAppIconImageSource  = appInfo.IconImage; break;
+			case AppPosition.Bottom: BottomAppIconImageSource = appInfo.IconImage; break;
 		}
 	}
 
-	/// <summary>
-	/// 선택된 영역의 프로그램을 실행
-	/// </summary>
 	private void LaunchSelectedApp()
 	{
-		var configuration = _configurationService.GetConfiguration<RegisteredApplicationModel>();
+		var config = _configurationService.GetConfiguration<RegisteredApplicationModel>();
+		if (config is null) return;
 
-        if (configuration != null)
-        {
-			if      (IsSelectedLeft  ) _launchAppService.LaunchApp(configuration.LeftAppInformation.AppPath!  );
-			else if (IsSelectedTop   ) _launchAppService.LaunchApp(configuration.TopAppInformation.AppPath!   );
-			else if (IsSelectedRight ) _launchAppService.LaunchApp(configuration.RightAppInformation.AppPath! );
-			else if (IsSelectedBottom) _launchAppService.LaunchApp(configuration.BottomAppInformation.AppPath!);
-		}        
+		var position = GetSelectedPosition();
+		if (position is null) return;
+
+		var path = config.GetByPosition(position.Value).AppPath;
+		if (!string.IsNullOrEmpty(path))
+			_launchAppService.LaunchApp(path);
 	}
 
-	#endregion
+	private AppPosition? GetSelectedPosition()
+	{
+		if (IsSelectedLeft)   return AppPosition.Left;
+		if (IsSelectedTop)    return AppPosition.Top;
+		if (IsSelectedRight)  return AppPosition.Right;
+		if (IsSelectedBottom) return AppPosition.Bottom;
+		return null;
+	}
 }
