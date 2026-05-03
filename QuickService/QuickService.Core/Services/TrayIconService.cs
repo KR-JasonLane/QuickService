3eb5551ad19e4ba16636435d963bc3cc70504790
Lee Coder <98294800+KR-JasonLane@users.noreply.github.com>
2026-05-03 12:42:46 +0900

    REFACTOR : TrayIconService 리팩토링 - 메서드 분리 및 Process.Kill을 Shutdown으로 변경

--- QuickService/QuickService.Core/Services/TrayIconService.cs
+++ QuickService/QuickService.Core/Services/TrayIconService.cs
@@ -1,94 +1,64 @@
-﻿using QuickService.Abstract.Interfaces;
+using QuickService.Abstract.Interfaces;
 
 namespace QuickService.Core.Services;
 
 public class TrayIconService : ITrayIconService
 {
+	private readonly NotifyIcon _trayIcon;
+
 	public TrayIconService()
 	{
-		CreateTrayIconObject();
+		_trayIcon = CreateTrayIcon();
 	}
 
-	#region Properties
-
 	/// <summary>
-	/// 윈도우를 Hide시켰을 때 생성 될 트레이 아이콘.
+	/// 작업표시줄에 트레이아이콘 표시 여부 설정
 	/// </summary>
-	private NotifyIcon? _trayIcon;
-
-	#endregion
-
-	#region Methods
-
-	/// <summary>
-	/// 작업표시줄에 트레이아이콘을 생성
-	/// </summary>
-	/// <param name="isVisible"> 트레이아이콘 표시 여부 </param>
 	public void VisibleTrayIconOnTaskBar(bool isVisible)
 	{
-		_trayIcon!.Visible = isVisible;
+		_trayIcon.Visible = isVisible;
 	}
 
-	/// <summary>
-	/// 트레이 아이콘 객체 생성
-	/// </summary>
-	private void CreateTrayIconObject()
+	private NotifyIcon CreateTrayIcon()
 	{
-		Icon	icon		= Properties.Resources.QuickService;
-		string	processName	= Process.GetCurrentProcess().ProcessName;
-		Window	mainWindow	= Application.Current.MainWindow;
-
-		//프로젝트명에 포함되어있는 '.' 이후 글자 삭제
-		int findDotIndex = processName.IndexOf('.');
-
-		if (findDotIndex != -1)
-			processName = processName.Substring(0, findDotIndex);
-
-		_trayIcon = new()
+		var icon = new NotifyIcon
 		{
-			Icon	= icon,
-			Text	= processName,
-			Visible = false
+			Icon    = Properties.Resources.QuickService,
+			Text    = GetApplicationName(),
+			Visible = false,
 		};
-
-		_trayIcon.DoubleClick += (sender, args) =>
-		{
-			mainWindow.Show();
-			mainWindow.WindowState = WindowState.Normal;
-			_trayIcon.Visible = false;
-		};
-
-		_trayIcon.ContextMenuStrip = GetTrayIconMenuStrip();
+		icon.DoubleClick += (_, _) => RestoreMainWindow();
+		icon.ContextMenuStrip = CreateContextMenu();
+		return icon;
 	}
 
-	/// <summary>
-	/// 메뉴 스트립 생성
-	/// </summary>
-	/// <returns> 생성 된 메뉴스트립 </returns>
-	private ContextMenuStrip GetTrayIconMenuStrip()
+	private static string GetApplicationName()
 	{
-		Window mainWindow = Application.Current.MainWindow;
-
-		ToolStripMenuItem menuItemOpen = new ToolStripMenuItem("Open");
-		menuItemOpen.Click += (sender, args) =>
-		{
-			_trayIcon!.Visible = false;
-			mainWindow.Show();
-		};
-
-		ToolStripMenuItem menuItemClose = new ToolStripMenuItem("Close");
-		menuItemClose.Click += (sender, args) =>
-		{
-			_trayIcon!.Visible = false;
-			Process.GetCurrentProcess().Kill();
-		};
-
-		ContextMenuStrip menuStrip = new ContextMenuStrip();
-		menuStrip.Items.Add(menuItemOpen);
-		menuStrip.Items.Add(menuItemClose);
-
-		return menuStrip;
+		var name = Process.GetCurrentProcess().ProcessName;
+		var dotIndex = name.IndexOf('.');
+		return dotIndex >= 0 ? name[..dotIndex] : name;
 	}
 
-	#endregion
+	private ContextMenuStrip CreateContextMenu()
+	{
+		var menu = new ContextMenuStrip();
+		menu.Items.Add(CreateMenuItem("Open", (_, _) => RestoreMainWindow()));
+		menu.Items.Add(CreateMenuItem("Close", (_, _) => Application.Current.Shutdown()));
+		return menu;
+	}
+
+	private static ToolStripMenuItem CreateMenuItem(string text, EventHandler handler)
+	{
+		var item = new ToolStripMenuItem(text);
+		item.Click += handler;
+		return item;
+	}
+
+	private void RestoreMainWindow()
+	{
+		_trayIcon.Visible = false;
+		var window = Application.Current.MainWindow;
+		window.Show();
+		window.WindowState = WindowState.Normal;
+	}
 }
